d657cd00129583c6898b1825bdf2072e5bc0aece
Lin Jiang <lnjng07@protonmail.com>
2024-10-16 10:07:23 -0700

    new http server for rest api

--- srv/src/main.rs
+++ srv/src/main.rs
@@ -1,16 +1,30 @@
+use actix_web::{middleware, App, HttpServer};
 use diesel::prelude::*;
+use diesel::r2d2::ConnectionManager;
 use dotenvy::dotenv;
-use std::env;
+use std::{env, io};
 
 pub mod models;
 pub mod schema;
 
-fn main() {
+#[actix_web::main]
+async fn main() -> io::Result<()> {
     // establish environment variables
     dotenv().ok();
 
     // connect to the database
     let database_url: String = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let mut connection: PgConnection = PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = diesel::r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    // create a new API server
+    HttpServer::new(move || {
+        App::new()
+            .app_data(pool.clone())
+            .wrap(middleware::Logger::default())
+    }).bind("0.0.0.0:9000")?
+        .run()
+        .await
 }
